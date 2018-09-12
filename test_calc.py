0da8d49f8303999d7afe8847c0ef1e7b9cf98fc0
JefersonCEST <42880288+JefersonCEST@users.noreply.github.com>
2018-09-11 21:54:54 -0300

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -1,7 +1,7 @@
 import pytest
 from calc import soma, sub, multi, divi
 
-@pytest.mark.parametrize('a,b,esp', [(1,1,2),(1,2,3),(2,3,5)])
+@pytest.mark.parametrize('a,b,esp', [(1,1,2),(1,2,3),(2,3,5), ('2',3,5)])
 def test_soma(a,b,esp):
  assert soma(a,b) == esp
 
